f75add96fb12347f6fd5bea19a864d5332c9f73b
Nagarajan Mariappan <NagarajanM@sulekha.com>
2020-07-25 10:36:53 +0530

    movie fix

---
+++ precache-manifest.bd5a537376bd150f046063c9f89ee620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294d2e07a61eb2f927b7b864ed9d9fa6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a77f741c833e8c82c654",
+    "url": "./static/css/main.5a1430da.chunk.css"
+  },
+  {
+    "revision": "93e22d0556cd61bda83b",
+    "url": "./static/js/2.d2fdd23a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.d2fdd23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77f741c833e8c82c654",
+    "url": "./static/js/main.5d8b34b8.chunk.js"
+  },
+  {
+    "revision": "4fd61bc9ef64ad7766df",
+    "url": "./static/js/runtime-main.6e244aa0.js"
+  },
+  {
+    "revision": "a6b97ecb540e6e097a8aa3961ffe84de",
+    "url": "./static/media/loading.a6b97ecb.gif"
+  }
+]);
